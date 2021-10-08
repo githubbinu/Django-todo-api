fb73c71ac2179d23bd8a2466fd5afc05b7cf16f2
githubbinu <binuisi2020@gmail.com>
2021-10-08 18:24:13 +0530

    urlchange

--- api/urls.py
+++ api/urls.py
@@ -9,7 +9,7 @@ urlpatterns=[
     path('notes/<str:pk>/update/',views.updateNote),
 
     path('notes/<str:pk>/',views.getNote),
-    path('notes/<str:pk>/delete/',views.deleteNote),
+    path('notes/delete/<str:pk>',views.deleteNote),
 
     
 ]
